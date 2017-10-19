f9807d08709251a73094434e7c85b957b13df96f
Chris Kundra (Mac) <ckundra@meditech.com>
2017-10-19 11:42:02 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+/*jshint node: true, esversion: 6*/
+'use strict';
+
+const fs = require('fs');
+
+const Hapi = require('hapi');
+const Good = require('good');
+const Boom = require('boom');
+const Joi = require('joi');
+
+const jwt = require('jsonwebtoken');
+
+const server = new Hapi.Server();
+server.connection({ port: 8000, host: 'localhost' });
+
+server.route({
+    method: 'POST',
+    path: '/v1/jwt-generator',
+    config: {
+        validate: {
+            payload: {
+                attributes: Joi.object().required(),
+                certificate: Joi.string().required(),
+                parameters: Joi.object({
+                    algorithm: Joi.string().valid('RS256').required(),
+                    expiresIn: Joi.string().required(),
+                    notBefore: Joi.string().required()
+                })
+            }
+        }
+    },
+    handler: function (request, reply) {
+
+        const certName = './certificates/' + request.payload.certificate + '.pem';
+
+        fs.readFile(certName , null, function(err, certificate) {
+
+            if (err) {
+
+                reply(Boom.internal('Certificate not found'));
+
+            } else {
+
+                const cert = {
+                    key: certificate
+                };
+
+                const options = {
+                    algorithm: request.payload.parameters.algorithm,
+                    expiresIn: request.payload.parameters.expiresIn,
+                    notBefore: request.payload.parameters.notBefore
+                };
+
+                reply({'jwt': jwt.sign(request.payload.attributes, cert, options)});
+
+            }
+        });
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
